532dc26ad69d931b87c7a0722acde5443c897113
Ashish <Ashish@192.168.1.5>
2017-07-30 18:32:02 +0530

    Custom Exception and Mapper

---
+++ AptiWithAB-API/src/main/java/com/aptiwithabapi/exceptions/DataNotFoundException.java
@@ -0,0 +1,14 @@
+package com.aptiwithabapi.exceptions;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8631483062937824809L;
+
+	public DataNotFoundException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
---
+++ AptiWithAB-API/src/main/java/com/aptiwithabapi/exceptions/DataNotFoundExceptionMapper.java
@@ -0,0 +1,19 @@
+package com.aptiwithabapi.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class DataNotFoundExceptionMapper implements ExceptionMapper<DataNotFoundException> {
+
+	@Override
+	public Response toResponse(DataNotFoundException exception) {
+		// TODO Auto-generated method stub
+		return Response.status(Status.NOT_FOUND)
+				.entity(exception)
+				.build();
+	}
+
+}
